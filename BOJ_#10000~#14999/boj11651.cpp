cd09dc1075d0ab4ccabff5a8dca35981a0f867f2
Minty U <deathcat@outlook.kr>
2022-10-09 22:46:25 +0900

    [NEW] boj#11651 upload

---
+++ BOJ_#10000~#14999/boj11651.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct pos
+{
+    int x, y;
+};
+bool comp(pos a, pos b);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n, x, y;
+    cin >> n;
+    pos *arr = new pos[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x >> y;
+        arr[i] = {x, y};
+    }
+    sort(arr, arr + n, comp);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i].x << " " << arr[i].y << "\n";
+    }
+
+    return 0;
+}
+
+bool comp(pos a, pos b)
+{
+    if (a.y == b.y)
+    {
+        return a.x < b.x;
+    }
+    else
+    {
+        return a.y < b.y;
+    }
+}
